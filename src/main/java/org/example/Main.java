ade437b630f82a9c1308c37c7d0a5cf2903e7a77
sallesalex01 <alex.salles2007@gmail.com>
2023-12-17 19:07:12 -0300

    Happyge

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,130 +3,86 @@ package org.example;
 import com.google.gson.Gson;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
     static Gson gson = new Gson();
+    static Timer timer = new Timer();
 
     public static void main(String[] args) {
-        Gson gson = new Gson();
+        JFrame frame = new JFrame("Controle de Acesso");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        frame.setLayout(new FlowLayout());
+        JLabel codigoLabel = new JLabel("Codigo de acesso: ");
+        JTextField codigoTextField = new JTextField(20);
 
+        codigoTextField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                String resposta = sendRequestPost(codigoTextField.getText());
+                Data respostaServer = gson.fromJson(resposta, Data.class);
 
+                if ("1".equals(respostaServer.ACK)){
+                    frame.getContentPane().setBackground(Color.GREEN);
+                    resetarCor(frame);
 
-            JFrame frame = new JFrame("Cliente");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(600,400);
-            frame.setLocationRelativeTo(null);
-
-            JPanel panel = new JPanel();
-            panel.setLayout(new GridLayout(7,2));
-
-//            JTextField inputField1 = new JTextField();
-//            inputField1.setFont((new Font("Arial", Font.PLAIN, 30)));
-//
-//            JLabel label1 = new JLabel("Nome:");
-//            label1.setFont((new Font("Arial", Font.BOLD, 30)));
-//
-
-
-            JLabel massaLabel = new JLabel("Massa:");
-            massaLabel.setFont((new Font("Arial", Font.BOLD, 30)));
-            JTextField massa = new JTextField();
-            massa.setFont((new Font("Arial", Font.PLAIN, 30)));
-
-
-            JLabel alturaLabel = new JLabel("Altura:");
-            alturaLabel.setFont((new Font("Arial", Font.BOLD, 30)));
-            JTextField altura = new JTextField();
-            altura.setFont((new Font("Arial", Font.PLAIN, 30)));
-
-
-            JLabel responseLabel = new JLabel("Resposta: ");
-            responseLabel.setFont((new Font("Arial", Font.BOLD, 30)));
-            JTextField responseField = new JTextField();
-            responseField.setFont((new Font("Arial", Font.PLAIN, 30)));
-
-
-
-            panel.add(massaLabel);
-            panel.add(massa);
-            panel.add(alturaLabel);
-            panel.add(altura);
-            panel.add(responseLabel);
-            panel.add(responseField);
-
-            String[] buttonLabels ={
-                    "ENVIAR", "SAIR"
-            };
-
-            for (String label: buttonLabels){
-                JButton button = new JButton(label);
-                button.setFont(new Font("Arial", Font.PLAIN, 30));
-                panel.add(button);
-
-                button.addActionListener(new ActionListener() {
-
-                    public void actionPerformed(ActionEvent e) {
-
-                        try {
-
-                            String response = sendRequestPost(massa.getText(), altura.getText());
-
-                            responseField.setText(response);
-
-                            if (label.equals("SAIR")) {
-                                System.exit(0);
-                            }
-
-                        }
-                        catch (Exception er){
-                            System.out.println(er);
-                        }
-
-                    }
-                });
-            }
-            frame.add(panel);
-            frame.setVisible(true);
-        }
-
-
-    public static String sendRequest(String nome) {
-        try {
-            String apiUrl = "http://localhost:3000/api/" + nome;
-            URL url = new URL(apiUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuilder response = new StringBuilder();
-
-            while ((inputLine = reader.readLine()) != null) {
-                response.append(inputLine);
+                } else {
+                    frame.getContentPane().setBackground(Color.RED);
+                    resetarCor(frame);
+                }
             }
 
-            reader.close();
-            connection.disconnect();
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                String resposta = sendRequestPost(codigoTextField.getText());
+                Data respostaServer = gson.fromJson(resposta, Data.class);
 
-            return response.toString();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return null;
+                if ("1".equals(respostaServer.ACK)){
+                    frame.getContentPane().setBackground(Color.GREEN);
+                    resetarCor(frame);
+
+                } else {
+                    frame.getContentPane().setBackground(Color.RED);
+                    resetarCor(frame);
+                }
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                String resposta = sendRequestPost(codigoTextField.getText());
+                Data respostaServer = gson.fromJson(resposta, Data.class);
+
+                if ("1".equals(respostaServer.ACK)){
+                    frame.getContentPane().setBackground(Color.GREEN);
+                    resetarCor(frame);
+                } else {
+                    frame.getContentPane().setBackground(Color.RED);
+                    resetarCor(frame);
+                }
+            }
+        });
+
+        frame.add(codigoLabel);
+        frame.add(codigoTextField);
+
+        frame.setVisible(true);
     }
 
-    public static String sendRequestPost(String massa, String altura) {
+
+    public static String sendRequestPost(String codigo) {
         try {
-            String apiUrl = "http://localhost:3000/api/imc";
+            String apiUrl = "http://localhost:3000/api/verificarCodigo";
             URL url = new URL(apiUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
@@ -134,7 +90,7 @@ public class Main {
             connection.setDoOutput(true);
 
             // Construa os dados a serem enviados no corpo da requisição em formato JSON
-            String jsonInputString = "{\"massa\": \"" + massa + "\", \"altura\": \"" + altura + "\"}";
+            String jsonInputString = "{\"codigo\": \"" + codigo + "\"}";
 
             // Escreva os dados no corpo da requisição usando OutputStream
             try (OutputStream os = connection.getOutputStream()) {
@@ -162,15 +118,23 @@ public class Main {
     }
 
     private static class Data{
-        double massa;
-        double altura;
+        String ACK;
 
-        Data(Double massa, double altura){
-            this.massa = massa;
-            this.altura = altura;
+        Data(String ACK){
+           this.ACK = ACK;
         }
     }
 
 
+
+    private static void resetarCor(JFrame frame) {
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // Altere a cor de volta ao normal
+                frame.getContentPane().setBackground(UIManager.getColor("Panel.background"));
+            }
+        }, 3000);
+    }
 
 }
